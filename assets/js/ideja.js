e36c497792ac366b914826d990f03005c6d9665c
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-11-26 18:50:56 +0200

    Delete ideja.js

--- assets/js/ideja.js
+++
@@ -1,9 +0,0 @@
-/* Open navbar when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav2").style.width = "100%";
-  }
-  
-  /* Close navbar when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav2").style.width = "0%";
-  }
