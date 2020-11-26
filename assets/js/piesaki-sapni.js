f6c776b55d86e72c3ae1bdf3884687818ca51aea
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-11-26 18:50:42 +0200

    Delete piesaki-sapni.js

--- assets/js/piesaki-sapni.js
+++
@@ -1,9 +0,0 @@
-/* Open navbar when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav3").style.width = "100%";
-  }
-  
-  /* Close navbar when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav3").style.width = "0%";
-  }
