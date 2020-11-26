f7ba3282741bc86cc0121206dcebb5dd4b8cf41b
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-11-26 22:52:48 +0200

    Add files via upload

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -8,41 +8,44 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
-const span = document.getElementById("section5");
-span.style.top = document.body.offsetHeight - document.getElementById("footer").offsetHeight - window.innerHeight + "px";
 
 
-$(document).ready(function () {
-  // Add smooth scrolling
+function smoothScroll() {
+  var element = document.getElementById("section5");
+element.style.top = document.body.offsetHeight - document.getElementById("footer").offsetHeight - window.innerHeight + "px";
+  element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+// $(document).ready(function () {
+//   // Add smooth scrolling
   
-  $(".fixed-anchor-link").on("click", function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
+//   $(".fixed-anchor-link").on("click", function (event) {
+//     // Make sure this.hash has a value before overriding default behavior
+//     if (this.hash !== "") {
+//       // Prevent default anchor click behavior
       
-      event.preventDefault();
+//       event.preventDefault();
       
-      // Store hash
-      var hash = this.hash;
+//       // Store hash
+//       var hash = this.hash;
         
-      //let scrollAnount =  $(hash).position().top - $(hash).height();
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate( 
-        {
-          scrollTop: $(hash).offset().top,
+//       //let scrollAnount =  $(hash).position().top - $(hash).height();
+//       // Using jQuery's animate() method to add smooth page scroll
+//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//       $("html, body").animate( 
+//         {
+//           scrollTop: $(hash).offset().top,
           
-        },
-        3000,
-        function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
+//         },
+//         3000,
+//         // function () {
+//         //   // Add hash (#) to URL when done scrolling (default click behavior)
+//         //   window.location.hash = hash;
+//         // }
         
-      );
-    } // End if
-  });
-});
+//       );
+//     } // End if
+//   });
+// });
 
 
 
@@ -59,9 +62,9 @@ window.addEventListener('scroll', function(e) {
   }
   this.oldScroll = this.scrollY;
 })
+// document.getElementById('upArrow').style.display = "block"  
 
-
- document.getElementById('upArrow').addEventListener('click', function(){
-  window.location = '#'
-})
+//  document.getElementById('upArrow').addEventListener('click', function(){
+//   smoothScroll();
+// })
 
